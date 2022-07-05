3f7d6fb21a2ea6140bc3bcd99e40f7c2dd3f8cbe
Tadashi-hue <remagnavana@gmail.com>
2022-07-05 23:18:56 +0100

    initialized

---
+++ asset-manager/src/test/java/com/manavana/assetmanager/AssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manavana.assetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
